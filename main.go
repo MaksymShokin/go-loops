a074310cb6f156b9d060a26bb8f03e91acb2d058
Maksym <maksym.sh@upplabs.com>
2021-09-11 22:56:47 +0300

    switch

--- main.go
+++ main.go
@@ -25,9 +25,22 @@ func main() {
 
 
 
-	if parsedAge > 18 {
+	if parsedAge > 18 && parsedAge < 65 {
 		fmt.Println("Welcome to the club")
 	} else if parsedAge < 18 {
 		fmt.Println("Sorry, you are too young")
+	} else {
+		fmt.Println("Good over 65")
 	}
+
+	// switch parsedAge {
+	// case "1":
+	// 		fmt.Println("Option 1")
+	// case "2":
+	// 		fmt.Println("Option 2")
+	// case "3":
+	// 		fmt.Println("Option 3")
+	// default:
+	// 		fmt.Println("Invalid choice!")
+	// }
 }
