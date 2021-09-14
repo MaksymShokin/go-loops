a2109183a4d5a798f8ecb67b3f440f37a1f1e021
Maksym <maksym.sh@upplabs.com>
2021-09-14 11:30:04 +0300

    set-up

---
+++ loops.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func parseInput(input string) string {
+	if runtime.GOOS == "windows" {
+		input = strings.Replace(input, "\r\n", "", -1)
+	} else {
+		input = strings.Replace(input, "\n", "", -1)
+	}
+
+	return input
+}
+
+func getInputNumber() (int, error) {
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		return 0, err
+	}
+
+	input = parseInput(input)
+	chosenInput, err := strconv.ParseInt(input, 0, 64)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(chosenInput), nil
+}
+
+func main() {
+	selectedChoice, err := getUserChoice()
+
+	if err != nil {
+		fmt.Println("Invalid choice, exiting!")
+	}
+
+	switch selectedChoice {
+	case "1":
+		calculateSumUpToNumber()
+
+	case "2":
+		findTheFactorial()
+
+	case "3":
+		sumNumbers()
+
+	case "4":
+		sumList()
+
+	}
+}
+
+func calculateSumUpToNumber() {
+	chosenNumber, err := getInputNumber()
+
+}
+
+func findTheFactorial() {}
+func sumNumbers()       {}
+func sumList()          {}
+
+func getUserChoice() (string, error) {
+	fmt.Println("Please enter your choice:")
+	fmt.Println("1) Add all numbers up to number X")
+	fmt.Println("2) Find the factorial of number X")
+	fmt.Println("3) Sum up the selected numbers")
+	fmt.Println("4) Sum up a list of numbers")
+
+	userChoice, err := reader.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	userChoice = parseInput(userChoice)
+
+	if userChoice == "1" || userChoice == "2" || userChoice == "3" || userChoice == "4" {
+		return userChoice, nil
+	}
+
+	return "", errors.New("user choice is incorrect")
+
+}
